2563041f110650c4e4817983c9ca720e4bb4b480
Rupal2905 <140495819+Rupal2905@users.noreply.github.com>
2025-03-01 16:24:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.write('An interactive visualization tool for Relative Rotation Graphs (RRG)')
 # Initialize data
 @st.cache_data(ttl=3600)  # Cache data for 1 hour
 def fetch_data(period='1y'):
-    tickers = ['^CNXAUTO', '^CNXFMCG', '^CNXIT', '^CNXREALTY', '^CNXCONSUM', '^CNXPHARMA', 'NIFTY_MIDCAP_100.NS' 
+    tickers = ['^CNXAUTO', '^CNXFMCG', '^CNXIT', '^CNXREALTY', '^CNXCONSUM', '^CNXPHARMA', 'NIFTY_MIDCAP_100.NS', 
                '^CNXMETAL', '^CNXENERGY', '^CNXMEDIA', '^CNXINFRA', '^CNXPSUBANK', '^NSEBANK', 'NIFTY_MID_SELECT.NS']
     benchmark = '^NSEI'
 
